74781a07375fdfb062db4aa62ab7179bd0b45d37
Dennis Flannigan <DLFlann@gmail.com>
2017-08-30 15:53:46 -0500

    Refactor routes out in to their own files. Additonally, add 404 error handling in app.js

--- app.js
+++ app.js
@@ -9,45 +9,22 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-    console.log('Hello');
-    next();
-});
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
-    console.log('World');
-    next();
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', { name });
-    } else {
-        res.redirect('/hello');
-    }
-});
-
-app.get('/cards', (req, res) => {
-    res.render('card', {prompt: "Who is burried in Grant's tomb?"});
-});
-
-app.get('/hello', (req, res) => {
-    if (req.cookies.username) {
-        res.redirect('/')
-    } else {
-        res.render('hello');
-    }
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(8000, () => {
